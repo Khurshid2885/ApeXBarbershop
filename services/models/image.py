e49b72ae06b161cfdc14fc4469566e445535d052
Xurshid <khurshid.khbek@gmail.com>
2025-04-30 14:33:32 +0500

    Tested barber panel

--- services/models/image.py
+++ services/models/image.py
@@ -7,3 +7,6 @@ from services.models.service import Service
 class HaircutImage(BaseModel):
     service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name="haircuts")
     img_file = models.FileField(upload_to='haircuts/', default="haircuts/crew_cut.png", blank=True, null=True)
+
+    class Meta:
+        db_table = 'haircutImage'
